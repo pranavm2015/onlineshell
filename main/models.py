361f63361ed72d8f20dc94677837cc31a432e38c
Pranav Mutharia <pranav.mutharia@gmail.com>
2018-07-12 18:20:17 +0530

    color

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class user(models.Model):
+    uid = models.IntegerField(null=True)
+    email = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        db_table = "user";
+	
--- main/views.py
+++ main/views.py
@@ -3,20 +3,109 @@ import paramiko
 import spur
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from .models import user
 # Create your views here.
 
 @csrf_exempt
 def home (request) :
-	read_data = ""
-	with open('main/lastcmd.txt', 'r') as f:
-		read_data = f.read()
-	dat = read_data.split(" ")
-	try:
-		context = {"ip":dat[0], "usernm":dat[1], "pass":dat[2]}
-	except Exception as e :
-		context= {}
-	print(context)
-	return render (request, "home.html", context)
+	if request.method == 'GET':
+		if 'email' in request.session:
+			return render(request, 'cmd.html', {})
+		else:
+			return render(request, 'home.html', {})
+
+	if request.method == 'POST':
+		print(request.POST)
+		form = request.POST
+		if 'email' in form :
+			print(form['email'])
+			context = { "message":"" }
+			try :
+				save  = user.objects.get(email=form['email'])
+			except :
+				context["message"] = "Email doesn't exist"
+				return render(request, 'home.html', context)
+
+			if form['password'] == save.password :
+				request.session['email'] = save.email
+				read_data = ""
+				with open('main/lastcmd.txt', 'r') as f:
+					read_data = f.read()
+				dat = read_data.split(" ")
+				try:
+					context = {"ip":dat[0], "usernm":dat[1], "pass":dat[2]}
+				except Exception as e :
+					context= {}
+				print(context)
+				return render (request, "cmd.html", context)
+			context["message"] = "Incorrect  Password"
+			return render(request, 'home.html', context)
+		else:
+			try:
+				del request.session['email']
+			except:
+				pass
+			return render(request, 'home.html', {})
+
+
+
+
+
+
+	# if 'email' in request.session:
+	# 	if request.method == 'POST' :
+	# 		try:
+	# 			del request.session['email']
+	# 		except:
+	# 			pass
+	# 		return render(request, 'home.html', {})
+			    
+	# 	return render(request, 'cmd.html', {})
+	
+	# else:
+	# 	if request.method == 'POST':
+	# 		print(request.POST)
+	# 		form = request.POST
+	# 		if 'email' in form :
+	# 			print(form['email'])
+	# 			context = { "message":"" }
+	# 			try :
+	# 				save  = user.objects.get(email=form['email'])
+	# 			except :
+	# 				context["message"] = "Email doesn't exist"
+	# 				return render(request, 'home.html', context)
+
+	# 			if form['password'] == save.password :
+	# 				request.session['email'] = save.email
+	# 				read_data = ""
+	# 				with open('main/lastcmd.txt', 'r') as f:
+	# 					read_data = f.read()
+	# 				dat = read_data.split(" ")
+	# 				try:
+	# 					context = {"ip":dat[0], "usernm":dat[1], "pass":dat[2]}
+	# 				except Exception as e :
+	# 					context= {}
+	# 				print(context)
+	# 				return render (request, "dash.html", context)
+	# 			context["message"] = "Incorrect  Password"
+	# 			return render(request, 'home.html', context)
+
+	# 	if request.method == 'GET':
+	# 		return render(request, 'home.html', {})
+
+
+# @csrf_exempt
+# def cmdline (request) :
+# 	read_data = ""
+# 	with open('main/lastcmd.txt', 'r') as f:
+# 		read_data = f.read()
+# 	dat = read_data.split(" ")
+# 	try:
+# 		context = {"ip":dat[0], "usernm":dat[1], "pass":dat[2]}
+# 	except Exception as e :
+# 		context= {}
+# 	print(context)
+# 	return render (request, "home.html", context)
 
 @csrf_exempt
 def runcmd (request) :
